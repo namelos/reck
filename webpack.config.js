30731937f1b1e258583d4236c378e6fa8ce822b5
Fred Wang <namelos@outlook.com>
2016-04-12 10:48:34 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const webpack = require('webpack')
+const BrowserSync = require('browser-sync-webpack-plugin')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/entry',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new BrowserSync({
+      host: 'localhost',
+      port: 3001,
+      proxy: 'http://localhost:3000'
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      loader: 'babel',
+      include: path.join(__dirname, 'src')
+    }]
+  }
+}
