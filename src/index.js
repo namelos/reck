0e4b4fd444ab9bd8c07a0127793122d16ddf856d
Fred Wang <namelos@outlook.com>
2016-04-14 11:21:43 +0800

    add create reducer

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import {combineReducers, applyMiddleware, createStore, compose} from 'redux'
 import {Provider} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-import {createAction, createReducer} from 'redux-act'
 import {reducer as form} from 'redux-form'
 import DevTools from './DevTools'
 
-const baseReducer = (state = {}, action) => {
-  switch (action.type) {
-    default:
+const createReducer = (initialState, handlers) =>
+  (state = initialState, action) => {
+    if (handlers.hasOwnProperty(action.type))
+      return handlers[action.type](state, action.payload)
+    else
       return state
   }
-}
+
+/* --- */
 
 const bindReducers = reducers =>
-  combineReducers({baseReducer, ...reducers})
+  combineReducers({form, ...reducers})
 
-const enhancer = compose(
-  DevTools.instrument()
-)
+const enhancer = compose(DevTools.instrument())
 
-const store = createStore(bindReducers())
+const store = enhancer(createStore)(bindReducers())
 store.reducers = {}
 
 store.addReducer = (name, reducer) => {
@@ -30,50 +30,35 @@ store.addReducer = (name, reducer) => {
   store.replaceReducer(bindReducers(store.reducers))
 }
 
-render(
-  <Provider store={store}>
-    <div>
-
-    </div>
-  </Provider>,
-  document.querySelector('#app')
-)
-
-// const counter = createReducer({
-//   increment: state => state + 1,
-//   decrement: state => state - 1
-// }, 0)
-
-// store.addReducer('counter', counter)
-
-const reconnect = (reducerName, initialState, handler) => Element => {
-  const reducer = createReducer(handler, initialState)
-
-  store.addReducer(reducerName, reducer)
-
-  const actionTypes = Object.keys(handler)
-  actionTypes.pop()
-
-  const actions = {}
-
-  for(let key in actionTypes) {
-    const key = actionTypes[key]
-    const type = key.toUpperCase()
-    const action = createAction(type)
-    actions[key] = actions
-  }
+const createDecorator = store => {
+  // store.addReducer('counter', (state = 0, action) => {
+  //   switch (action.type) {
+  //     case 'INCREMENT':
+  //       return state + 1
+  //     case 'DECREMENT':
+  //       return state - 1
+  //     default:
+  //       return state
+  //   }
+  // })
+  store.addReducer('counter', createReducer(0, {
+    increment: state => state + 1,
+    decrement: state => state - 1
+  }))
 
   return connect(
     state => state,
-    dispatch => bindActionCreators({ actions }, dispatch)
+    dispatch => bindActionCreators({
+      increment: () => ({type: 'increment'}),
+      decrement: () => ({type: 'decrement'})
+    }, dispatch)
   )
 }
 
-const Counter = reconnect('counter', 0, {
-  increment: state => state + 1,
-  decrement: state => state - 1
-})
-(({ counter, increment, decrement }) => {
+const decorator = createDecorator(store)
+
+const Counter = decorator
+(({counter, increment, decrement}) => {
   return (
     <div>
       <p>{ counter }</p>
@@ -85,8 +70,10 @@ const Counter = reconnect('counter', 0, {
 
 render(
   <Provider store={store}>
-    <Counter />
+    <div>
+      <Counter />
+      <DevTools />
+    </div>
   </Provider>,
   document.querySelector('#app')
 )
-
